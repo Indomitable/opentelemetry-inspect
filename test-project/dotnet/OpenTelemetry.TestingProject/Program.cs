108f605136fd072c31fa69af4a13133288f11748
vmladenov <v.mladenov@hotmail.com>
2026-01-05 16:28:58 +0100

    feat: add testing meters in the dotnet app.

--- test-project/dotnet/OpenTelemetry.TestingProject/Program.cs
+++ test-project/dotnet/OpenTelemetry.TestingProject/Program.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using OpenTelemetry;
 using OpenTelemetry.Exporter;
 using OpenTelemetry.Resources;
@@ -28,10 +29,14 @@ builder.Services.AddOpenTelemetry()
         tb.AddSource(activitySourceName);
     })
     .WithLogging()
+    .WithMetrics(mb =>
+    {
+        mb.AddMeter(activitySourceName);
+    })
     .UseOtlpExporter(string.IsNullOrEmpty(protocol) || protocol == "grpc" ? OtlpExportProtocol.Grpc : OtlpExportProtocol.HttpProtobuf, new Uri(otlpAddress ?? "http://127.0.0.1:4318"));
 
 builder.Services.AddSingleton<ActivitySource>(_ =>  new ActivitySource(activitySourceName));
-
+builder.Services.AddSingleton<Meter>(_ => new Meter(activitySourceName));
 
 var app = builder.Build();
 
@@ -49,13 +54,34 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", (ActivitySource activitySource, ILogger<WeatherForecast> logger) =>
+app.MapGet("/weatherforecast", async (ActivitySource activitySource, Meter meter, ILogger<WeatherForecast> logger) =>
 {
     using var activity = activitySource.StartActivity("GetWeatherForecast", ActivityKind.Server, null, new Dictionary<string, object?>()
     {
         ["date"] = DateTimeOffset.UtcNow
     });
     logger.LogInformation("Weather forecast for {date}", DateTimeOffset.UtcNow);
+    
+    var historgram = meter.CreateHistogram<long>("TimeToFetch", "ms", "Time to fetch weather forecast",
+        new Dictionary<string, object?>
+        {
+            ["date"] = DateTimeOffset.UtcNow
+        });
+    
+    var counter = meter.CreateCounter<long>("CalledCount", "units", "How many times the endpoint is called", [
+        new KeyValuePair<string, object?>("Endpoint", "GetWeatherForecast")
+    ]);
+    counter.Add(1, new KeyValuePair<string, object?>("TemperatureC", 1));
+    
+    var upDown = meter.CreateUpDownCounter<int>("ProcessingRequests", "requests", "How many current requests are processed", [new KeyValuePair<string, object?>("test", true)]);
+    upDown.Add(1);
+    
+    var gauge = meter.CreateGauge<long>("CurrentMemory", "bytes", "How much memory is allocated.", [ new KeyValuePair<string, object?>("test", new Dictionary<string, string> { ["test"] = "1" })]);
+    var watch = Stopwatch.StartNew();
+    gauge.Record(GC.GetTotalAllocatedBytes());
+    
+    await Task.Delay(TimeSpan.FromMilliseconds(Random.Shared.Next(10, 2000)));
+    
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
@@ -64,6 +90,10 @@ app.MapGet("/weatherforecast", (ActivitySource activitySource, ILogger<WeatherFo
             summaries[Random.Shared.Next(summaries.Length)]
         ))
         .ToArray();
+    
+    upDown.Add(-1);
+    watch.Stop();
+    historgram.Record(watch.ElapsedMilliseconds, new KeyValuePair<string, object?>("status", 1));
     return forecast;
 })
 .WithName("GetWeatherForecast");
